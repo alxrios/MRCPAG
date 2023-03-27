561622f8e7154b7243ce4e1fae69c79e04fa71c1
Alejandro Ríos <76874671+alxrios@users.noreply.github.com>
2023-03-28 00:03:00 +0200

    feat: updated

--- Menu.cpp
+++ Menu.cpp
@@ -4,13 +4,14 @@
 
 
 void Menu::display(bool & end, shelf books) {
+	books.chargeInfo();
 	std::cout << "\nChoose an option:\n";
 	std::cout << "1. Write the file with input introduced by the console.\n";
 	std::cout << "2. Actualize current page of a book.\n";
 	std::cout << "3. Delete a book.\n";
 	std::cout << "4. Rename a book.\n";
 	std::cout << "5. Actualize the total number of pages of a book\n";
-	std::cout << "6. Print the into.\n";
+	std::cout << "6. Print the info.\n";
 	std::cout << "9. End program.\n";
 	int option;
 	std::cin >> option;
@@ -33,8 +34,20 @@ void Menu::display(bool & end, shelf books) {
 			int ind;
 			std::cin >> ind;
 			books.deleteBook(ind);
-			books.printInfo();
 			books.chargeInfo();
+			books.printInfo();
+			break;
+		case 4:
+			std::cout << "Which book do you want to rename? (introduce book index)\n";
+			int indfour;
+			std::cin >> indfour;
+			books.renameBook(indfour);
+			books.chargeInfo();
+			books.printInfo();
+			break;
+		case 5:
+			std::cout << "From which book do you want to update the current number of total pages? "
+				"(introduce book index)\n";
 			break;
 		case 6:
 			books.printInfo();
