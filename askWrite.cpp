b4ff005812ff9f088cb813c6c0e2ece330708880
Alejandro Ríos <76874671+alxrios@users.noreply.github.com>
2023-03-29 18:50:49 +0200

    feat: ask2 function added

--- askWrite.cpp
+++ askWrite.cpp
@@ -11,3 +11,14 @@ void askWrite::ask() {
 		writeClass::writeFile();
 	}
 }
+
+
+void askWrite::ask2() {
+	std::cout << "Do you wanna write the file 2 for the first time?: (y/n)\n";
+	char option = 'n';
+	std::cin >> option;
+	std::cin.ignore();
+	if (option == 'y') {
+		writeClass::writeFile2();
+	}
+}
