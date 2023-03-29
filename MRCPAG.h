3eb059236c2f542a778633183a1ae1488f547d71
Alejandro Ríos <76874671+alxrios@users.noreply.github.com>
2023-03-29 18:42:40 +0200

    feat: updated

--- MRCPAG.h
+++ MRCPAG.h
@@ -20,3 +20,22 @@ class shelf {
 		
 };
 
+
+class shelf2 : public shelf {
+	public:
+		shelf2();
+		void printInfo();
+		void chargeInfo();
+		void actualizeCpage();
+		void deleteBook(int);
+		void renameBook(int);
+	private:
+		std::string subject[30];
+		std::string names[30];
+		int currentPage[30];
+		int totalPages[30];
+		std::string filePath[30];
+		int length; //Same as previous arrays size
+		void writeFfile();
+};
+
