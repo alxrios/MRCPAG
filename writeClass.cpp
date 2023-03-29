70d41c939fbc2371598b8f7184a4cef2b57fe588
Alejandro Ríos <76874671+alxrios@users.noreply.github.com>
2023-03-30 00:21:16 +0200

    feat: updated shelf2

--- writeClass.cpp
+++ writeClass.cpp
@@ -40,12 +40,12 @@ void writeClass::writeFile() {
 void writeClass::writeFile2() {
 	std::cout << "Current books reading info...\n";
 	std::cout << "Please introduce subject, name, current page and total number of pages of each book\n\n";
-	std::string subject[12];
-	std::string names[12];
-	int currentPage[12];
-	int totalPages[12];
-	int length = 12;
-	std::string filePath[12];
+	std::string subject[30];
+	std::string names[30];
+	int currentPage[30];
+	int totalPages[30];
+	int length = 30;
+	std::string filePath[30];
 	for (int i = 0; i < length; i++) {
 		std::cout << i + 1 << ".Subject: ";
 		getline(std::cin, subject[i]);
