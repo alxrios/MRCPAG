71409295fc67bd5d44fc4bf7fa14bf12bbecb154
Alejandro Ríos <76874671+alxrios@users.noreply.github.com>
2023-04-10 00:32:59 +0200

    feat: minor changes in format

--- main.cpp
+++ main.cpp
@@ -7,33 +7,21 @@
 
 int main() {
 	
-	
 	shelf bookShelf;
 	shelf2 bookShelf2;
-	
 	
 	bool end = false;
 	while (!end) {
 		Menu::display(end, bookShelf, bookShelf2);
 	}
 	
-	
-	
 	return 0;
 }
-
-
-//Notes add at the first of the file the 
-//info with the number of rows in the table
-// Example: number of items: 8 or current length
-// Read funciton must skip this line
-
-
-// Note 4: make a txt file which contains paths of .exe applications for
-// Easy launch with PowerShell. Ex: firefox.exe path.
-
 
 // Note 5: Add types control in each input by the user to make the program less breakable.
 
 // Note 6: When ask (y/n) question add control of other letters other than y n.
 
+// Note 7: Write a function to easy append a book to the end of shelf (no shelf2).
+// outfile.open("test.txt", std::ios_base::app); // append instead of overwrite
+// Put "\n" after "user input";
